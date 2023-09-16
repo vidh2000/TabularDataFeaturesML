8af96c28d7e1aa03483791e95e8326774b28e016
vidh2000 <vid.homsak19@imperial.ac.uk>
2023-09-16 16:26:13 +0200

    Clustering with K-means successful and accurate

--- clustering.py
+++ clustering.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from preprocessing import *
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
-
+from sklearn.cluster import KMeans
+import numpy as np
 
 data = loadData()
 
@@ -27,11 +28,10 @@ X_reduced = pca.fit_transform(X)
 #                                             test_size=0.3,random_state=312)
 
 # PLOT data
-
 df = pd.DataFrame(X_reduced)
 df["species"] = data["species"]
 df["species_id"] = data["species_id"]
-print(df.head())
+
 # Define colors for each class
 colors = {0: 'red', 1: 'blue', 2: 'green'}
 
@@ -45,4 +45,24 @@ plt.xlabel('Feature 1')
 plt.ylabel('Feature 2')
 plt.legend()
 plt.grid(True)
+
+# Clustering with K-Means
+kmeans = KMeans(n_clusters=3, n_init="auto").fit(X)
+ypred = kmeans.labels_
+df["kmeans predictions"] = ypred
+
+accuracy = float(sum((ypred == df["species_id"])))/len(ypred)
+print(accuracy)
+
+# Create a scatter plot with different colors for each class
+plt.figure(figsize=(8, 6))
+for label, color in colors.items():
+    subset = df[df["kmeans predictions"] == label]
+    plt.scatter(subset[0], subset[1], c=color, label=f'Species No. {label+1}', marker='o', s=50)
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.legend()
+plt.grid(True)
+
+
 plt.show()
